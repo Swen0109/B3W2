b05d6f34bc41f870d522f75c0bf3deb9c87bd6a1
Swen <99058148@mydavinci.nl>
2020-02-19 09:53:29 +0100

    Commit2

---
+++ Lab1-2/welcome.php
@@ -0,0 +1,4 @@
+<?php 
+	print("Naam: $_GET[Naam] <br>");
+	print("Email: $_GET[Email] <br>");
+?>
---
+++ Lab3/lab3.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="welcome.css">
+	<title></title>
+</head>
+<body>
+	<form class="indeling" action="lab3.php" method="POST">
+		<h1>De ingevulde gegevens zijn:</h1>
+		Name:<input type="text" name="Naam"><br>
+		Email:<input type="email" name="Email">
+		<input type="submit" name="verzenden" id="klaar">
+	</form>
+
+</body>
+</html>
+
+<?php
+	print('Naam $_POST[Naam] <br>');
+	print('Email $_POST[Email] <br>');
+	
+?>
