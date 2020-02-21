c39bfe5e536fde380d90a069593608a9073aa088
Swen <99058148@mydavinci.nl>
2020-02-21 11:39:50 +0100

    Commit5
    Laatste commit

--- Mad libs/madlibs.php
+++ Mad libs/madlibs.php
@@ -1,16 +1,28 @@
-<?php
-
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="madlibs.css">
 	<title></title>
 </head>
-<body>
+<body id="body2">
+	<img src="afbeeldingen/madlibs.png" id=banner>
+
+	<div id="twee2">
+		<h1 id="titel">Onkunde</h1>
+		<div class="verhaal">
+			<?php
+				print("Er zijn veel mensen die niet kunnen $_POST[vraag1] . Neem nou $_POST[vraag2]. Zelfs met de hulp van een $_POST[vraag4] of zelfs $_POST[vraag3] kan $_POST[vraag2] niet $_POST[vraag1]. Dat heeft niet te maken met een gebrek aan $_POST[vraag5], maar met een te veel aan $_POST[vraag6]. Te veel $_POST[vraag6] leidt tot $_POST[vraag7] en dat is niet goed als je wilt $_POST[vraag1]. Helaas voor $_POST[vraag2].")
+
+			?>
+		</div>
+		<div id= twee>
+			<p id="tekst">Er heerst paniek... &nbsp &nbsp &nbsp &nbsp &nbsp Onkunde</p>
+		</div>
+
+		<div id="drie2">
+			<p id=footertekst>Deze website is gemaakt door Spinoza</p>
+		</div>
+	</div>
 
 </body>
 </html>
---
+++ Mad libs/madlibs2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="madlibs.css">
+	<title></title>
+</head>
+<body id="body2">
+	<img src="afbeeldingen/madlibs.png" id=banner>
+
+	<div id="twee3">
+		<h1 id="titel">Er heerst paniek...</h1>
+		<div class="verhaal2">
+			<?php
+				print("Er heerst paniek in het koninkrijk $_POST[vraag3]. Koning $_POST[vraag6] is ten einde raad en als koning $_POST[vraag6] ten einde raad is, dan roept hij zijn ten-einde-raadsheer $_POST[vraag2].<br><br> '$_POST[vraag2]! Het is een ramp! Het is een schande!'<br><br> 'Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?' <br><br> 'Mijn $_POST[vraag1] is verdwenen! Zo maar, zonder waarschuwing. En ik had net $_POST[vraag5] voor hem gekocht!' '<br><br> Majesteit, uw $_POST[vraag1] komt vast vanzelf weer terug?' <br><br> 'Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd $_POST[vraag8] leren?' <br><br> 'Maar Sire, daar kunt u toch uw $_POST[vraag7] voor gebruiken.' <br><br> '$_POST[vraag2], je hebt helemaal gelijk! Wat zou ik doen als ik jouw niet had.' <br><br> ' $_POST[vraag4], Sire.'")
+
+			?>
+		</div>
+		<div id= twee>
+			<p id="tekst">Er heerst paniek... &nbsp &nbsp &nbsp &nbsp &nbsp Onkunde</p>
+		</div>
+	</div>
+
+</body>
+</html>
